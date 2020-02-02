e3b089f6fc48d24d47a4feaf3b80e7ba9bcc42ea
nimish <nimishsantosh107@icloud.com>
2020-02-02 14:55:10 +0530

    updated

--- server/script.js
+++ server/script.js
@@ -2,4 +2,7 @@ var client = new WebTorrent();
 
 seedButton.addEventListener('click',function () {
     console.log(seedInput.files);
+    client.seed(seedInput.files, function (torrent) {
+        console.log('SEEDING ' + torrent.magnetURI);
+    });
 });

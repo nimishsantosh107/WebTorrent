e3ae01ddbb27ad506583c615ed2a8e88d85e632c
nimish <nimishsantosh107@icloud.com>
2020-02-03 22:53:41 +0530

    bundled

--- app/server/script.js
+++ app/server/script.js
@@ -6,11 +6,11 @@ var client = new WebTorrent();
 
 seedButton.addEventListener('click',function () {
     console.log(seedInput.files);
-    client.seed(seedInput.files, { announce: ["http://localhost:6000/announce",
-                                              "udp://0.0.0.0:6000", 
-                                              "udp://localhost:6000", 
-                                              "ws://localhost:6000",
-                                              "http://localhost:6000/stats"]}, function (torrent) {
+    client.seed(seedInput.files, { announce: ["http://localhost:8000/announce",
+                                              "udp://0.0.0.0:8000", 
+                                              "udp://localhost:8000", 
+                                              "ws://localhost:8000",
+                                              "http://localhost:8000/stats"]}, function (torrent) {
         console.log('SEEDING: ' + torrent.magnetURI);
     });
 });

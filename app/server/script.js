d64d6ea8c592b325ebf46a6aa6eda117b5e3e882
nimish <nimishsantosh107@icloud.com>
2020-02-05 09:56:46 +0530

    latest

--- app/server/script.js
+++ app/server/script.js
@@ -4,13 +4,14 @@ const dragDrop = require('drag-drop');
 
 var client = new WebTorrent();
 
-seedButton.addEventListener('click',function () {
+seedButton.addEventListener('click', function () {
     console.log(seedInput.files);
-    client.seed(seedInput.files, { announce: ["http://localhost0.localhost:6000/announce",
-                                              "udp://0.0.0.0:6000", 
-                                              "udp://localhost:6000",
-                                              "ws://localhost:6000",
-                                              "http://localhost:6000/stats"]}, function (torrent) {
+    client.seed(seedInput.files, {
+        announce:  ["http://192.168.0.104:6000/announce",
+                    "udp://192.168.0.104:6000",
+                    "ws://192.168.0.104:6000"
+                    ]
+    }, function (torrent) {
         console.log('SEEDING: ' + torrent.magnetURI);
     });
 });
